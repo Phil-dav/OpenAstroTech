e42341c74a44c8aa5b56791c527f2a7dc585a2d0
Phil-dav <flophil34@yahoo.com>
2023-12-15 20:08:10 +0100

    ajout programmes de tests

--- OpenAstroTracker-Firmware/src/inc/Globals.cpp
+++
@@ -1,5 +0,0 @@
-#include "Globals.hpp"
-
-// TODO: Should move this somewhere else...
-bool inSerialControl      = false;  // True when the serial port is in control
-bool inNorthernHemisphere = true;   // Default to northern hemisphere
